a1c3477fe8ecc77b878c25e6d52618617f7a87b3
zhulangren <zhulangren@gmail.com>
2015-07-12 21:33:19 -0700

    移动进程存在检测到python里边

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
+#-*- coding:utf-8 -*-
 from websocket_server import WebsocketServer
 import subprocess
-
+import os
 # Called for every client connecting (after handshake)
 def new_client(client, server):
 	print("New client connected and was given id %d" % client['id'])
@@ -17,12 +18,17 @@ cmd_dic={"1":'./tool/longtime.sh'}
 
 def message_dispatch(client,message):
 	try:
-		if(cmd_dic.has_key(message)):
-			subp=subprocess.Popen("./tool/start.sh "+cmd_dic[message],shell=True,stdout=subprocess.PIPE)
-			while subp.poll()==None:
-			    server.send_message(client,subp.stdout.readline())
+		pids= os.popen("ps -ef | grep %s |grep -v grep | awk '{print $2}'" % (cmd_dic[message]))
+		pids=pids.read();
+		if(pids==""):
+			if(cmd_dic.has_key(message)):
+				subp=subprocess.Popen(cmd_dic[message],shell=True,stdout=subprocess.PIPE)
+				while subp.poll()==None:
+				    server.send_message(client,subp.stdout.readline())
+			else:
+				server.send_message(client,"key don't exist!!!\n")
 		else:
-			server.send_message(client,"key don't exist!!!\n")
+			server.send_message(client,"进程已经在运行了，请等待执行完成!!!\n")		
 	except Exception as e:
 			print("ERROR: message_dispatch: "+str(e))
 # Called when a client sends a message
